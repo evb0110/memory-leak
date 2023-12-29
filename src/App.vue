cef76d369ae8c4cb8e002e4e735f761508d9a906
Eugene Barsky <7149553@gmail.com>
2023-12-29 12:57:28 +0300

    initial

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="app">
+        <button @click="isShowTest = !isShowTest">
+            {{ isShowTest ? 'Hide' : 'Show' }}
+        </button>
+    </div>
+    <Test v-if="isShowTest" />
+</template>
+
+<script>
+import Test from './components/Test.vue';
+
+export default {
+    name: 'App',
+    components: {
+        Test,
+    },
+    data() {
+        return {
+            isShowTest: false,
+        };
+    },
+};
+</script>
+
+<style>
+#app {
+    text-align: center;
+    margin-top: 60px;
+}
+</style>
---
+++ src/components/Test.vue
@@ -0,0 +1,29 @@
+<template>
+    <h1>Test</h1>
+    <div>{{ testData.name }}</div>
+    <div>{{ testGetter.name }}</div>
+</template>
+
+<script>
+
+class TestData {
+    name = 'data';
+}
+class TestGetter {
+    name = 'getter';
+}
+
+export default {
+    name: 'Test',
+    data() {
+        return {
+            testData: new TestData(),
+        };
+    },
+    computed: {
+        testGetter() {
+            return new TestGetter();
+        },
+    },
+};
+</script>
